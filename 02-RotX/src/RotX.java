4f729391ff4916f657b3f638fcecbc1bed2c604f
2627juancamilobenitez-ai <2627_juancamilo.benitez@iticbcn.cat>
2026-09-25 17:27:32 +0200

    02 ROTX final clase

--- 02-RotX/src/RotX.java
+++ 02-RotX/src/RotX.java
@@ -1,11 +1,11 @@
 public class RotX{
 
-        char[] miniscules =  {'a','á','à','b','c','ç','d','e','é','è','f','g','h','i','í','ì','ï','j','k','l','m','n',
-                'ñ','q','r','s','t','u','ú','ù','ü','v','w','x','y','z'};
+        char[] minuscules =  {'a','á','à','b','c','ç','d','e','é','è','f','g','h','i','í','ì','ï','j','k','l','m','n',
+                'ñ','o','ó','ò','p','q','r','s','t','u','ú','ù','ü','v','w','x','y','z'};
 
-        char[] majuscules = {'A','Á','À','B','C','Ç','D','E','É','È','F','G','H','I','Í','Ì','Ï','J','K','L','M','N','Ñ','Q','R','S','T','U','Ú','Ù','Ü','V','W','X','Y','Z'};
+        char[] majuscules = {'A','Á','À','B','C','Ç','D','E','É','È','F','G','H','I','Í','Ì','Ï','J','K','L','M','N','Ñ','O','Ó','Ò','P','Q','R','S','T','U','Ú','Ù','Ü','V','W','X','Y','Z'};
 
-        public String xifratRot13 (String cadena){
+        public String xifratRotX (String cadena, int desplaçament){
 
             String resultat = "";
 
@@ -17,7 +17,7 @@ public class RotX{
 
                 for(int j = 0; j < majuscules.length; j++){
                     if(lletra == majuscules[j]){
-                        int posicioNova = (j + 13) % majuscules.length;
+                        int posicioNova = (j + desplaçament) % majuscules.length;
                         char lletraXifrada = majuscules[posicioNova];
                         resultat += lletraXifrada;
                         trobada = true;
@@ -25,10 +25,10 @@ public class RotX{
 
                 }
 
-                for(int k = 0; k < miniscules.length; k++){
-                    if(lletra == miniscules[k]){
-                        int posicioNova = (k + 13) % miniscules.length;
-                        char lletraXifrada = miniscules[posicioNova];
+                for(int k = 0; k < minuscules.length; k++){
+                    if(lletra == minuscules[k]){
+                        int posicioNova = (k + desplaçament) % minuscules.length;
+                        char lletraXifrada = minuscules[posicioNova];
                         resultat += lletraXifrada;
                         trobada = true;
                     }
@@ -45,7 +45,7 @@ public class RotX{
 
         }
 
-        public String desXifratRot13(String cadena) {
+        public String desXifratRotX(String cadena, int desplaçament) {
 
             String resultat = "";
 
@@ -56,7 +56,7 @@ public class RotX{
 
                 for (int j = 0; j < majuscules.length; j++) {
                     if (lletra == majuscules[j]) {
-                        int posicioNova = (j - 13 + majuscules.length) % majuscules.length;
+                        int posicioNova = (j - desplaçament + majuscules.length) % majuscules.length;
                         char lletraXifrada = majuscules[posicioNova];
                         resultat += lletraXifrada;
                         trobada = true;
@@ -64,10 +64,10 @@ public class RotX{
 
                 }
 
-                for (int k = 0; k < miniscules.length; k++) {
-                    if (lletra == miniscules[k]) {
-                        int posicioNova = (k - 13 + miniscules.length) % miniscules.length;
-                        char lletraXifrada = miniscules[posicioNova];
+                for (int k = 0; k < minuscules.length; k++) {
+                    if (lletra == minuscules[k]) {
+                        int posicioNova = (k - desplaçament + minuscules.length) % minuscules.length;
+                        char lletraXifrada = minuscules[posicioNova];
                         resultat += lletraXifrada;
                         trobada = true;
                     }
@@ -83,6 +83,37 @@ public class RotX{
             return resultat;
 
         }
+
+        public String forcaBrutaRotX(String cadenaXifrada){
+            String resultat = "";
+            
+
+            for (int i = 0; i < cadenaXifrada.length() ;i++ ){
+                char lletra = cadenaXifrada.charAt(i);
+       
+                for(int j = 0; j < 40; j++){
+                int posicio = (lletra + j) % majuscules.length ;
+                char lletraXifrada =  majuscules[posicio];
+                resultat += lletraXifrada;
+                }
+
+            }
+
+            for(int i = 0; i < cadenaXifrada.length(); i++){
+                char lletra = cadenaXifrada.charAt(i);
+
+                for(int j = 0; j < 40; j++){
+                    int posicio = (lletra + j) % minuscules.length;
+                    char lletraXifrada = minuscules[posicio];
+                    resultat += lletraXifrada;
+                }
+            }
+
+
+            return resultat;
+        }
+
+
             void main(String [] args) {
 
                 String frase1 = "ABC";
@@ -93,24 +124,29 @@ public class RotX{
                 System.out.println("Xifrat");
                 System.out.println("--------");
 
-                System.out.printf("%-20s> %s%n", frase1, xifratRot13(frase1));
-                System.out.printf("%-20s> %s%n", frase2, xifratRot13(frase2));
-                System.out.printf("%-20s> %s%n", frase3, xifratRot13(frase3));
-                System.out.printf("%-20s> %s%n", frase4, xifratRot13(frase4));
+                System.out.printf("%-20s> %s%n", frase1, xifratRotX(frase1, 0));
+                System.out.printf("%-20s> %s%n", frase2, xifratRotX(frase2, 2));
+                System.out.printf("%-20s> %s%n", frase3, xifratRotX(frase3, 4));
+                System.out.printf("%-20s> %s%n", frase4, xifratRotX(frase4, 6));
 
                 System.out.println();
                 System.out.println("Desxifrat");
                 System.out.println("---------");
 
-                String fraseXifrada1 = xifratRot13(frase1);
-                String fraseXifrada2 = xifratRot13(frase2);
-                String fraseXifrada3 = xifratRot13(frase3);
-                String fraseXifrada4 = xifratRot13(frase4);
+                String fraseXifrada1 = xifratRotX(frase1, 0);
+                String fraseXifrada2 = xifratRotX(frase2, 2);
+                String fraseXifrada3 = xifratRotX(frase3, 4);
+                String fraseXifrada4 = xifratRotX(frase4, 6);
 
-                System.out.printf("%-20s => %s%n", fraseXifrada1, desXifratRot13(fraseXifrada1));
-                System.out.printf("%-20s => %s%n", fraseXifrada2, desXifratRot13(fraseXifrada2));
-                System.out.printf("%-20s=> %s%n", fraseXifrada3, desXifratRot13(fraseXifrada3));
-                System.out.printf("%-20s=> %s%n", fraseXifrada4, desXifratRot13(fraseXifrada4));
+                System.out.printf("%-20s => %s%n", fraseXifrada1, desXifratRotX(fraseXifrada1, 0));
+                System.out.printf("%-20s => %s%n", fraseXifrada2, desXifratRotX(fraseXifrada2, 2));
+                System.out.printf("%-20s=> %s%n", fraseXifrada3, desXifratRotX(fraseXifrada3, 4));
+                System.out.printf("%-20s=> %s%n", fraseXifrada4, desXifratRotX(fraseXifrada4, 6));
+
+                System.out.println("Missatge Xifrat " + fraseXifrada4);
+                System.out.println("---------");
+                System.out.println(forcaBrutaRotX(fraseXifrada4));
+
             }
 
 
